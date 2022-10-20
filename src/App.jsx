6edb442a4898e96848e3d9313f6bd56e93c93a01
Eshita <eshita.sapariya@gmail.com>
2022-10-20 11:38:36 +0530

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const app = () => {
+  return (
+    <>
+      <h1>Welcome to React Vite Micro App!</h1>
+      <p>Hard to get more minimal than this React app.</p>
+      <small>Hello this is me!!</small>
+    </>
+  );
+};
+export default app;
+// export default () => (
+//   <>
+//     <h1>Welcome to React Vite Micro App!</h1>
+//     <p>Hard to get more minimal than this React app.</p>
+//   </>
+// );
