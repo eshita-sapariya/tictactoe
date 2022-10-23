9d8c11c914ae692b42a11acb23d0bb1961121d1c
Eshita <eshita.sapariya@gmail.com>
2022-10-23 13:11:42 +0530

    styled entire app

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 
 const History = ({ history, moveTo, currentMove }) => {
   return (
-    <ul>
-      {history.map((_, move) => {
-        return (
-          <li key={move}>
-            {/* {console.log(move)}
+    <div className="history-wrapper">
+      <ul className="history">
+        {history.map((_, move) => {
+          return (
+            <li key={move}>
+              {/* {console.log(move)}
             {console.log(_)} */}
-            <button
-              style={{
-                fontWeight: move === currentMove ? 'bold' : 'normal',
-              }}
-              type="button"
-              onClick={() => {
-                moveTo(move);
-              }}
-            >
-              {move === 0 ? 'Go to game start' : `Go to move #${move}`}
-            </button>
-          </li>
-        );
-      })}
-    </ul>
+              <button
+                className={`btn-move ${move === currentMove ? 'active' : ''}`}
+                type="button"
+                onClick={() => {
+                  moveTo(move);
+                }}
+              >
+                {move === 0 ? 'Go to game start' : `Go to move #${move}`}
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
